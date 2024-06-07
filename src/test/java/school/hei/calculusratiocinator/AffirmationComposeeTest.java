5b04486076c7308e58b30e432ab50b97fb104717
josiaJj <hei.josia@gmail.com>
2024-06-07 17:01:15 +0300

    fix: modify variables to var

--- src/test/java/school/hei/calculusratiocinator/AffirmationComposeeTest.java
+++ src/test/java/school/hei/calculusratiocinator/AffirmationComposeeTest.java
@@ -7,25 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class AffirmationComposeeTest {
     @Test
     public void lou_est_pauvre_et_lou_est_genereux() {
-        Affirmation louEstPauvre = new AffirmationSimple("Lou est pauvre");
-        Affirmation louEstGenereux = new AffirmationSimple("Lou est généreux");
+        var louEstPauvre = new AffirmationSimple("Lou est pauvre");
+        var louEstGenereux = new AffirmationSimple("Lou est généreux");
 
-        Affirmation louEstPauvreEtGenereux = new AffirmationComposee(louEstPauvre, ConjonctionDeCoordination.ET, louEstGenereux);
-        ValeurDeVerite valeurAttendue = ValeurDeVerite.faux;
-        ValeurDeVerite valeurActuel = louEstPauvreEtGenereux.calculerValeurDeVerite();
+        var louEstPauvreEtGenereux = new AffirmationComposee(louEstPauvre, ConjonctionDeCoordination.ET, louEstGenereux);
+        var valeurAttendue = ValeurDeVerite.faux;
+        var valeurActuel = louEstPauvreEtGenereux.calculerValeurDeVerite();
         assertEquals(valeurActuel, valeurAttendue);
     }
 
     @Test
     public void lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre() {
-        Affirmation louEstBeau = new AffirmationSimple("Lou est beau");
-        Affirmation louEstPauvre = new AffirmationSimple("Lou est pauvre");
-        Affirmation louEstGenereux = new AffirmationSimple("Lou est généreux");
+        var louEstBeau = new AffirmationSimple("Lou est beau");
+        var louEstPauvre = new AffirmationSimple("Lou est pauvre");
+        var louEstGenereux = new AffirmationSimple("Lou est généreux");
 
-        Affirmation louEstBeauOuLouEstGenereux = new AffirmationComposee(louEstBeau, ConjonctionDeCoordination.OU, louEstGenereux);
-        Affirmation lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre = new AffirmationComposee(louEstBeauOuLouEstGenereux , ConjonctionDeCoordination.DONC, louEstPauvre);
-        ValeurDeVerite valeurAttendue = ValeurDeVerite.faux;
-        ValeurDeVerite valeurActuel = lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre.calculerValeurDeVerite();
+        var louEstBeauOuLouEstGenereux = new AffirmationComposee(louEstBeau, ConjonctionDeCoordination.OU, louEstGenereux);
+        var lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre = new AffirmationComposee(louEstBeauOuLouEstGenereux , ConjonctionDeCoordination.DONC, louEstPauvre);
+        var valeurAttendue = ValeurDeVerite.faux;
+        var valeurActuel = lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre.calculerValeurDeVerite();
         assertEquals(valeurActuel, valeurAttendue);
     }
 }
