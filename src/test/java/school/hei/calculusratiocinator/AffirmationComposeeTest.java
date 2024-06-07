1a46fe5c934489bc227538be3e5930a79acb787e
josiaJj <hei.josia@gmail.com>
2024-06-07 16:54:28 +0300

    feat: add lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre

--- src/test/java/school/hei/calculusratiocinator/AffirmationComposeeTest.java
+++ src/test/java/school/hei/calculusratiocinator/AffirmationComposeeTest.java
@@ -7,13 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class AffirmationComposeeTest {
     @Test
     public void lou_est_pauvre_et_lou_est_genereux() {
-        // Affirmation louEstBeau = new AffirmationSimple("Lou est beau");
         Affirmation louEstPauvre = new AffirmationSimple("Lou est pauvre");
         Affirmation louEstGenereux = new AffirmationSimple("Lou est généreux");
 
         Affirmation louEstPauvreEtGenereux = new AffirmationComposee(louEstPauvre, ConjonctionDeCoordination.ET, louEstGenereux);
         ValeurDeVerite valeurAttendue = ValeurDeVerite.faux;
         ValeurDeVerite valeurActuel = louEstPauvreEtGenereux.calculerValeurDeVerite();
+        assertEquals(valeurActuel, valeurAttendue);
+    }
+
+    @Test
+    public void lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre() {
+        Affirmation louEstBeau = new AffirmationSimple("Lou est beau");
+        Affirmation louEstPauvre = new AffirmationSimple("Lou est pauvre");
+        Affirmation louEstGenereux = new AffirmationSimple("Lou est généreux");
+
+        Affirmation louEstBeauOuLouEstGenereux = new AffirmationComposee(louEstBeau, ConjonctionDeCoordination.OU, louEstGenereux);
+        Affirmation lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre = new AffirmationComposee(louEstBeauOuLouEstGenereux , ConjonctionDeCoordination.DONC, louEstPauvre);
+        ValeurDeVerite valeurAttendue = ValeurDeVerite.faux;
+        ValeurDeVerite valeurActuel = lou_est_beau_ou_lou_est_genereux_donc_lou_est_pauvre.calculerValeurDeVerite();
         assertEquals(valeurActuel, valeurAttendue);
     }
 }
