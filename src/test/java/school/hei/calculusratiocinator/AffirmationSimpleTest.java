396bace4e95b3da322737d78d4015ddf7dcccc20
josiaJj <hei.josia@gmail.com>
2024-06-07 16:59:03 +0300

    fix: modify var scope

--- src/test/java/school/hei/calculusratiocinator/AffirmationSimpleTest.java
+++ src/test/java/school/hei/calculusratiocinator/AffirmationSimpleTest.java
@@ -7,25 +7,25 @@ import static org.junit.Assert.assertEquals;
 public class AffirmationSimpleTest {
     @Test
     public void lou_est_beau() {
-        Affirmation louEstBeau = new AffirmationSimple("Lou est beau");
-        ValeurDeVerite valeurActuel = louEstBeau.calculerValeurDeVerite();
-        ValeurDeVerite valeurAttendue = ValeurDeVerite.vrai;
+        var louEstBeau = new AffirmationSimple("Lou est beau");
+        var valeurActuel = louEstBeau.calculerValeurDeVerite();
+        var valeurAttendue = ValeurDeVerite.vrai;
         assertEquals(valeurActuel, valeurAttendue);
     }
 
     @Test
     public void lou_est_pauvre() {
-        Affirmation louEstPauvre = new AffirmationSimple("Lou est pauvre");
-        ValeurDeVerite valeurActuel = louEstPauvre.calculerValeurDeVerite();
-        ValeurDeVerite valeurAttendue = ValeurDeVerite.faux;
+        var louEstPauvre = new AffirmationSimple("Lou est pauvre");
+        var valeurActuel = louEstPauvre.calculerValeurDeVerite();
+        var valeurAttendue = ValeurDeVerite.faux;
         assertEquals(valeurActuel, valeurAttendue);
     }
 
     @Test
     public void lou_est_genereux() {
-        Affirmation louEstGenereux = new AffirmationSimple("Lou est généreux");
-        ValeurDeVerite valeurActuel = louEstGenereux.calculerValeurDeVerite();
-        ValeurDeVerite valeurAttendue = ValeurDeVerite.jenesaispas;
+        var louEstGenereux = new AffirmationSimple("Lou est généreux");
+        var valeurActuel = louEstGenereux.calculerValeurDeVerite();
+        var valeurAttendue = ValeurDeVerite.jenesaispas;
         assertEquals(valeurActuel, valeurAttendue);
     }
 
